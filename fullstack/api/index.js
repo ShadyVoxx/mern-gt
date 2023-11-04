b61ea98e64e9c9812dc6ce7d1098c7f6652b1206
Naga Srinivasan <nagasrinivasan200@gmail.com>
2023-11-04 12:28:35 +0530

    Add Port

---
+++ fullstack/api/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+import jwt from 'jsonwebtoken';
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(4000,()=>{
+    console.log("Listening on Port 4000");
+})
