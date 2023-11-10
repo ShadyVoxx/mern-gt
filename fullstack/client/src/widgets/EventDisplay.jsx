96d1ebf6b9524c10f9d7ac741b6613de65286579
SVNitinSV <svnitinsv@gmail.com>
2023-11-10 18:53:50 +0530

    Add Icons

--- fullstack/client/src/widgets/EventDisplay.jsx
+++ fullstack/client/src/widgets/EventDisplay.jsx
@@ -3,13 +3,16 @@ import { FaUser } from "react-icons/fa";
 
 const EventDisplay = (props) => {
   return (
-    <div className=" p-3">
-        <h1>{props.event}</h1>
-        <p>{props.time}</p>
+    <div className=" p-3 border rounded-xl mt-2 hover:bg-gray-50  hover:border-gray-300">
+        <h1 className='font-semibold pb-1'>{props.event}</h1>
+        <p className='px-2 ml-1 border w-fit rounded-full'>{props.time}</p>
         {
-            
-            <FaUser className="text-gray-400"/>
-            
+            <div className='flex items-center gap-2 p-2'>
+            <FaUser className="text-orange-300"/>
+            <FaUser className="text-orange-300"/>
+            <FaUser className="text-red-300"/>
+            <FaUser className="text-red-300"/>
+            </div>
         }
       </div>
   )
